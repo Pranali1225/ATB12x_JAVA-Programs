b0d67dfd79eae3c7ded1521407ef38b1e59f41db
Pranali <shethpranali1998@gmail.com>
2025-05-15 12:18:42 +0530

    Identifiers rules

---
+++ src/ex_02_JavaBasic_Identifiers_Format/LAB001_$.java
@@ -0,0 +1,7 @@
+package ex_02_JavaBasic_Identifiers_Format;
+
+public class LAB001_$ {
+    public static void main(String[] args) {
+        System.out.println("class identifier acccept first letter capital & $ symbol");
+    }
+}
