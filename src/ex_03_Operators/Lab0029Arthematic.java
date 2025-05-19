c72c1336c22b476e1755ad7bbdadbdb98d707d70
Pranali <shethpranali1998@gmail.com>
2025-05-19 14:47:21 +0530

    variable and literals

---
+++ src/ex_03_Operators/Lab0029Arthematic.java
@@ -0,0 +1,9 @@
+package ex_03_Operators;
+
+public class Lab0029Arthematic {
+    public static void main(String[] args) {
+        int a=10;
+        int b=78;
+        System.out.println(a+b);
+    }
+}
